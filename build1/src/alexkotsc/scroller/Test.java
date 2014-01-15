6cafe4764746488cd040f4c0c7791aab88eb8539
AlexKotsc-LAPTOP <AlexKotsc@gmail.com>
2014-01-15 01:53:38 +0100

    Added Slick libraries and SimpleSlickGame demo

---
+++ build1/src/alexkotsc/scroller/Test.java
@@ -0,0 +1,12 @@
+package alexkotsc.scroller;
+
+/**
+ * Created by AlexKotsc on 15-01-14.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
